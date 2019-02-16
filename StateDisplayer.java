43d64add0fb7788662fe83778dd4ea204591478d
rsuppl1 <46825987+rsuppl1@users.noreply.github.com>
2019-02-16 12:33:16 -0600

    update to State Displayer
    changed the variable type and created a method for update checking

--- StateDisplayer.java
+++ StateDisplayer.java
@@ -11,11 +11,11 @@ public abstract class StateDisplayer {
     
     // This variable keeps track of the current State of the Displayer.
     // Set to private and can be gotten from getDisplayState() method
-    private Object currentState;
+    protected Object currentState;
      
     // This is suppose to be a have all the possible states in an easy access 
     // array and just changes the array index.
-    private final Object[] displayStates;
+    protected final Object[] displayStates;
     
     // These create Object references to all the possible states
     LogInPage LoginPageObj = new LogInPage();
@@ -36,16 +36,10 @@ public abstract class StateDisplayer {
         return currentState;
     }
     
-    public void Checking()
+    public void updateChecking()
     {
         currentState.displayPage();
-        UpdateDisplay();
-    }
-    // when called upon tells the current state to handle the update 
-    // caught by the state loop by calling the handleUpdate method
-    public void UpdateDisplay()
-    {
-        currentState = currentState.handleUpdate();
+        currentState.handleUpdate();
     }
     
     // both of these absract methods are implemneted in each page's class
