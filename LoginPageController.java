74b6723b0decdc8a1bbfb6e40b4d9f0568cc1b93
rsuppl1 <46825987+rsuppl1@users.noreply.github.com>
2019-02-17 14:01:47 -0600

    Update LoginPageController.java

--- LoginPageController.java
+++ LoginPageController.java
@@ -1,24 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bacpac;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author rsuppl1
  */
-public class LoginPageController {
+public class LoginPageController  {
     
     LogInPage LoggedIn = new LogInPage();
     
@@ -37,8 +39,19 @@ public class LoginPageController {
             
             @Override
             public void handle(ActionEvent event) {
-                LoggedIn.setLogInTrue();
-                System.out.println("login = true");
+                try {
+                    LoggedIn.setLogInTrue();
+                    System.out.println("login = true");
+                    String fxmlResource = "SplashPage.fxml";
+                    Parent panel = null;
+                    panel = FXMLLoader.load(getClass().getResource(fxmlResource));
+                    Scene scene = new Scene(panel);
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException ex) {
+                    Logger.getLogger(LoginPageController.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }); 
     }
