7e11d30f0de822686d30a911d0cdab66af4057ae
NanoAce <45291351+NanoAce@users.noreply.github.com>
2019-04-02 09:00:32 -0500

    Add dynamic buttons to drink pages

--- BacPacApp/app/src/main/java/com/example/bacpacapp/BeerActivity.java
+++ BacPacApp/app/src/main/java/com/example/bacpacapp/BeerActivity.java
@@ -1,7 +1,11 @@
 package com.example.bacpacapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class BeerActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class BeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer);
+        String names[]={"aBeer1","aBeer2","aBeer3","aBeer4"};
+        Button listOfDrinks[]=new Button[4];
+        LinearLayout LL = (LinearLayout)findViewById(R.id.buttonlayout);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        Button cancelButton=findViewById(R.id.cancel3);
+        cancelButton.setText("Cancel");
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancel();
+            }
+        });
+
+
+
+        for(int i=0;i<4;i++) { //should be a while loop
+            listOfDrinks[i] = new Button(this);
+            listOfDrinks[i].setText(names[i]);
+            LL.addView(listOfDrinks[i], params);
+        }
+
+
+    }
+    private void cancel() {
+        Intent intent = new Intent(BeerActivity.this, DrinksActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
