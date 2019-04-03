d355deedd23da5312b5bfbf0b9a22262f3bb7b8a
Lterro1 <46950510+Lterro1@users.noreply.github.com>
2019-04-02 21:10:07 -0500

    Fixed User Profile

--- BacPacApp/app/src/main/java/com/example/bacpacapp/UserProfile.java
+++ BacPacApp/app/src/main/java/com/example/bacpacapp/UserProfile.java
@@ -22,84 +22,62 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class UserProfile extends AppCompatActivity {
     GoogleSignInClient mGoogleSignInClient;
-    private TextInputEditText w;
+    private TextInputEditText weightTV;
     private TextView nameTV;
-    private TextView emailTV;
-    private TextInputEditText h;
+    private TextInputEditText heightTV;
     private TextView bmi;
     private Button calculate;
     private double height, weight;
 
     public UserProfile() {
-        weight = 170;
-        height = 68;
+        weight = 0;
+        height = 0;
     }
-
     public UserProfile(double weight, double height) {
         this.height = height;
         this.weight = weight;
     }
-
     private double calcBMI(double weight, double height) {
         return (703 * (weight / (height * height)));
     }
-
     public double getWeight() {
         return weight;
     }
-
+    public void setWeight(double weight){this.weight = weight;}
     public double getHeight() {
         return height;
     }
-
+    public void setHeight(double height){this.height = height;}
     public double getBMI() {
         return calcBMI(getWeight(), getHeight());
-    }
-
-    @Override
+    }@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        //nameTV = findViewById(R.id.name);
-        emailTV = findViewById(R.id.email);
+        nameTV = findViewById(R.id.name);
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(UserProfile.this);
         if (acct != null){
             String personName= acct.getDisplayName();
-            String personGivenName = acct.getGivenName();
-            String personFamilyName = acct.getFamilyName();
-            String personEmail = acct.getEmail();
-            String personId = acct.getId();
             nameTV.setText("Name: "+personName);
-            emailTV.setText("Email: "+personEmail);
         }
+        heightTV = findViewById(R.id.h);
+        weightTV = findViewById(R.id.w);
         calculate = findViewById(R.id.calc);
-
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                h = findViewById(R.id.h);
-                w = findViewById(R.id.w);
-                String he = h.getText().toString();
-                String we = w.getText().toString();
-
-                if (!he.isEmpty()) {
-                    if (!we.isEmpty()) {
-                        {
-                            double hei = Double.parseDouble(he);
-                            double wei = Double.parseDouble(we);
-                            UserProfile user = new UserProfile(wei, hei);
-                            bmi = findViewById(R.id.bmi);
-                            bmi.setText(String.valueOf(user.getBMI()));
-
-                        }
-                    }
-                }
+                double heightD = Double.parseDouble(heightTV.getText().toString());
+                double weightD = Double.parseDouble(weightTV.getText().toString());
+                setWeight(weightD);
+                setHeight(heightD);
+                bmi = findViewById(R.id.bmi);
+                bmi.setText(String.valueOf(getBMI()));
+                heightTV.setText(String.valueOf(height));
+                weightTV.setText(String.valueOf(weight));
             }
         });
-
     }
 }
